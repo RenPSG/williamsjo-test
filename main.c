f49f8b12dfb8acc0f0ed7091ba4189c3790c1c95
williamsjo-reninc <williamsjo@reninc.com>
2024-10-17 12:42:51 -0400

    add more memory leaks (#3)
    * add Makefile
    
    * add more memory leaks

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 
 int main(int argc, char *argv[]) {
   char *buf, *s = malloc(1024);
+  for (int i = 1; i < 100; i++) {
+    s = malloc(1024 + i);
+	}
   s = "Hello, world!\n";
   printf("%s", s);
   return 0;
