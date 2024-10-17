1ea99c47bb58367467f9a7ec3321dc422cb9c0a9
williamsjo-reninc <williamsjo@reninc.com>
2024-10-17 12:53:26 -0400

    add out of bounds write (#4)

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  char *buf, *s = malloc(1024);
+  char *buf, *s = malloc(1);
   for (int i = 1; i < 100; i++) {
     s = malloc(1024 + i);
 	}
-  s = "Hello, world!\n";
+  s = malloc(1);
+  s = "out of bounds write\n";
   printf("%s", s);
   return 0;
 }
